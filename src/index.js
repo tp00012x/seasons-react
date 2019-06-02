0e1b95d85a54f5865975bd3ec9bb50352ec0ee2a
Anthony Torres <anthony_torres01@yahoo.com>
2019-06-02 16:07:45 -0500

    Added component did mount

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null,
+            errorMessage: '',
+        };
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered!')
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
